c4ee4854b8a4c776dcf6b0fc92e45eb20c245c1e
awooley4978 <awooley4978@users.noreply.github.com>
2026-07-22 00:44:08 +0000

    fix: remove broken metro.config.js causing dependency resolution failure

--- metro.config.js
+++
@@ -1,5 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.cacheStores = [{ get: () => path.resolve(__dirname, '.metro-cache') }];
-module.exports = defaultConfig;
